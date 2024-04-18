82977138edaf93dc98e3dcf37ed534eebafdff7c
Iyanna Yapo <Iyannayapo123@gmail.com>
2024-04-18 17:51:30 -0400

    backend made

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const router = express.Router();
+app.use(cors());
+app.use(express.json()); // to parse JSON bodies
+
+
+
+// MongoDB connection string
+const conn_str = 'mongodb+srv://Admin:Password123@campusconnect.rnnii0k.mongodb.net/?retryWrites=true&w=majority&appName=CampusConnect';
+
+mongoose.set('strictQuery', false);
+mongoose.connect(conn_str)
+    .then(() => {
+        console.log(`MongoDB Connection Succeeded`);
+        const port = process.env.PORT || 8082;
+        // Start listening for requests only after the database connection is successful
+        app.listen(port, () => console.log(`Server running on port ${port}`));
+    })
+    .catch(err => {
+        console.log(`Error in DB Connection ${err}`);
+    });
+
+
+
+
