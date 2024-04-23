a9d2629eea927c5f8d6133a83e98f531b65e0e45
Iyanna Yapo <Iyannayapo123@gmail.com>
2024-04-23 19:04:56 -0400

    Header image fixed.

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Header.css';
+import logoImage from './Gemini_Generated_Image_5fxwhq5fxwhq5fxw.jpeg';
 
 function Header({ isLoggedIn, onLogout }) {
   const navigate = useNavigate();
@@ -14,12 +15,10 @@ function Header({ isLoggedIn, onLogout }) {
   return (
     <header className="header">
       <Link to="/" className="home-link">
-      <div className="logo-and-title"> {/* Add this wrapper */}
-      <img 
-        src="https://cdn.gencraft.com/prod/user/568c8dc4-2af3-4b88-9aac-bd41c2ca4b8a/cb54135b-f4f1-4d14-b4ad-5e6d0a5bce98/image/image0_0.jpg?Expires=1712696307&Signature=kj637aEIEULvArx51c02nfNSAhtIB7h2ZDaXzEp21f2~js2TE~b1oMxx6juTOX3i8A5ylhh3bg1BV0IQi8wX0Gkd3AJr13ESruPk7YxkJcFcPpp-pMvFZNpDSPiQQfv-6fZhoYsDOaqIYM5VamGg9bH2CSSf8173vggmkzUEJVsLaSB8oRDZGa~Tc-mNeboDNj3mjodoh7n9y8O1tGXrmkbgTt7tB0-StOom633lhQN1v--P7-WcA0scHXYcpb009gXF2wTuFDYDZ8m-FOKmrLWMlNRig6MgDImniVl60y81QRUCJGCHhuTB2PIHXFQHusQaiCZ~O8hFQpNNir-jnw__&Key-Pair-Id=K3RDDB1TZ8BHT8" 
-        alt="Campus Connect Logo" 
-        className="header-logo"
-      />
+      <div className="logo-and-title"> 
+      <img src="https://res.cloudinary.com/degakxo0q/image/upload/v1713913364/Gemini_Generated_Image_5fxwhq5fxwhq5fxw_pimsbl.jpg"
+      alt="Campus Connect Logo" 
+      className="header-logo" />
       <h1>Campus Connect</h1>
     </div>
       </Link>
